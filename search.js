71b8ef84ca4e34eeca9b33389c94394a6ed85b35
Yasuaki Uechi <uetchy@randompaper.co>
2017-04-11 20:26:42 +0900

    feat: add lib

---
+++ search.js
@@ -0,0 +1,12 @@
+const {initDatabase} = require('./lib/database')
+
+initDatabase((err, db) => {
+  if (err) throw err
+
+  const cursor = db.search(process.argv[2], (err, results) => {
+    if (err) throw err
+
+    console.log(results.totalHits);
+    console.log(results.hits.map(hit => JSON.parse(hit.document).full_name))
+  })
+})
