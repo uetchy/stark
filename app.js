fc0998601828a1024f78a3d8385cf4580fbf7eea
Yasuaki Uechi <y@uechi.io>
2017-10-20 23:17:17 +0900

    prettier

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const { initDatabase } = require('./lib/database')
-const ConfigStore = require('./lib/config')
-const GitHubAPI = require('./lib/api')
+const { initDatabase } = require('./lib/database');
+const ConfigStore = require('./lib/config');
+const GitHubAPI = require('./lib/api');
 
-const configStore = new ConfigStore()
+const configStore = new ConfigStore();
 
 async function init() {
-  const config = await configStore.load()
-  const apiClient = new GitHubAPI(config.githubToken)
+  const config = await configStore.load();
+  const apiClient = new GitHubAPI(config.githubToken);
 
   initDatabase((err, db) => {
-    if (err) throw err
+    if (err) throw err;
 
     apiClient.starredWithReadme(record => {
       db.add([record], err => {
         if (err) {
-          console.log('db error:', err)
-          return
+          console.log('db error:', err);
+          return;
         }
 
         configStore.save({
           lastStarred: record.starred_at,
-          lastUpdated: new Date()
-        })
+          lastUpdated: new Date(),
+        });
 
-        console.log('added to database', record.full_name)
-      })
-    })
-  })
+        console.log('added to database', record.full_name);
+      });
+    });
+  });
 }
 
-init()
+init();
--- src/lib/github/star.js
+++ src/lib/github/star.js
@@ -1,4 +1,4 @@
-const GitHubAPI = require('github')
+const GitHubAPI = require('github');
 
 function fetchStars() {
   const client = new GitHubAPI({
@@ -7,48 +7,48 @@ function fetchStars() {
     protocol: 'https',
     host: 'api.github.com', // should be api.github.com for GitHub
     headers: {
-      'user-agent': 'Stark' // GitHub is happy with a unique user agent
+      'user-agent': 'Stark', // GitHub is happy with a unique user agent
     },
     Promise: require('bluebird'),
     followRedirects: true, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-    timeout: 5000
-  })
+    timeout: 5000,
+  });
 
-  client.authenticate({ type: 'oauth', token: process.env.GITHUB_TOKEN })
+  client.authenticate({ type: 'oauth', token: process.env.GITHUB_TOKEN });
 
-  var starredRepos = []
+  var starredRepos = [];
 
   var req = client.activity.getStarredRepos(
     {
-      per_page: 100
+      per_page: 100,
     },
     getRepos
-  )
+  );
   function getRepos(err, res) {
     if (err) {
-      return false
+      return false;
     }
 
-    starredRepos = starredRepos.concat(res)
+    starredRepos = starredRepos.concat(res);
     if (client.hasNextPage(res)) {
       // client.getNextPage(res, getRepos)
       console.log(
         starredRepos.map(function(repo) {
-          return repo['full_name']
+          return repo['full_name'];
         })
-      )
-      console.log('starred repos: ' + starredRepos.length)
+      );
+      console.log('starred repos: ' + starredRepos.length);
     } else {
       console.log(
         starredRepos.map(function(repo) {
-          return repo['full_name']
+          return repo['full_name'];
         })
-      )
-      console.log('starred repos: ' + starredRepos.length)
+      );
+      console.log('starred repos: ' + starredRepos.length);
     }
   }
 }
 
 module.exports = {
-  fetchStars
-}
+  fetchStars,
+};
